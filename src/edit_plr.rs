3d40b79e0c7a2ec26fc1758da90928c50adc09a9
Simon Zheng <simz97@outlook.com>
2023-04-08 17:53:19 -0400

    Add appearance support

--- src/edit_plr.rs
+++ src/edit_plr.rs
@@ -58,7 +58,11 @@ pub fn deserialize_raw_to_struct_plr(data: Vec<u8>) -> Plr {
     let difficulty: i8 = PlrUnpacker::r_i8(&mut reader);
     let play_time: i64 = PlrUnpacker::r_i64(&mut reader);
 
-    let unknown2 = PlrUnpacker::r_bytes(&mut reader, 9);
+    let hair_style: i32 = PlrUnpacker::r_i32(&mut reader);
+    let hair_dye: u8 = PlrUnpacker::r_u8(&mut reader);
+    let hide_visual: u16 = PlrUnpacker::r_u16(&mut reader);
+    let hide_misc: u8 = PlrUnpacker::r_u8(&mut reader);
+    let gender: u8 = PlrUnpacker::r_u8(&mut reader);
 
     let stat_life: i32 = PlrUnpacker::r_i32(&mut reader);
     let stat_life_max: i32 = PlrUnpacker::r_i32(&mut reader);
@@ -66,11 +70,19 @@ pub fn deserialize_raw_to_struct_plr(data: Vec<u8>) -> Plr {
     let stat_mana_max: i32 = PlrUnpacker::r_i32(&mut reader);
     let extra_accessory: bool = PlrUnpacker::r_bool(&mut reader);
 
-    let unknown3 = PlrUnpacker::r_u8(&mut reader);
+    let done_dundefii_event: bool = PlrUnpacker::r_bool(&mut reader);
 
     let tax_money: i32 = PlrUnpacker::r_i32(&mut reader);
 
-    let unknown4 = PlrUnpacker::r_bytes(&mut reader, 38);
+    let hair_colour: i32 = PlrUnpacker::r_i32(&mut reader);
+    let skin_colour: i32 = PlrUnpacker::r_i32(&mut reader);
+    let eye_colour: i32 = PlrUnpacker::r_i32(&mut reader);
+    let shirt_colour: i32 = PlrUnpacker::r_i32(&mut reader);
+    let undershirt_colour: i32 = PlrUnpacker::r_i32(&mut reader);
+    let pants_colour: i32 = PlrUnpacker::r_i32(&mut reader);
+    let shoes_colour: i32 = PlrUnpacker::r_i32(&mut reader);
+
+    let unknown2 = PlrUnpacker::r_bytes(&mut reader, 10);
 
     let armor: [Equipment; 3] = [
         Equipment {id: PlrUnpacker::r_i32(&mut reader), prefix: PlrUnpacker::r_u8(&mut reader)},
@@ -86,7 +98,7 @@ pub fn deserialize_raw_to_struct_plr(data: Vec<u8>) -> Plr {
         Equipment {id: PlrUnpacker::r_i32(&mut reader), prefix: PlrUnpacker::r_u8(&mut reader)}
     ];
 
-    let unknown5 = PlrUnpacker::r_bytes(&mut reader, 20);
+    let unknown3 = PlrUnpacker::r_bytes(&mut reader, 20);
 
     let accessories_vanity: [Equipment; 6] = [
         Equipment {id: PlrUnpacker::r_i32(&mut reader), prefix: PlrUnpacker::r_u8(&mut reader)},
@@ -97,7 +109,7 @@ pub fn deserialize_raw_to_struct_plr(data: Vec<u8>) -> Plr {
         Equipment {id: PlrUnpacker::r_i32(&mut reader), prefix: PlrUnpacker::r_u8(&mut reader)}
     ];
 
-    let unknown6 = PlrUnpacker::r_bytes(&mut reader, 5);
+    let unknown4 = PlrUnpacker::r_bytes(&mut reader, 5);
 
     let dyes: [Equipment; 8] = [
         Equipment {id: PlrUnpacker::r_i32(&mut reader), prefix: PlrUnpacker::r_u8(&mut reader)},
@@ -110,7 +122,7 @@ pub fn deserialize_raw_to_struct_plr(data: Vec<u8>) -> Plr {
         Equipment {id: PlrUnpacker::r_i32(&mut reader), prefix: PlrUnpacker::r_u8(&mut reader)},
     ];
 
-    let unknown7 = PlrUnpacker::r_bytes(&mut reader, 10);
+    let unknown5 = PlrUnpacker::r_bytes(&mut reader, 10);
 
     let mut inventory1: [Item; 32] = [Item::default(); 32];
     let mut inventory2: [Item; 26] = [Item::default(); 26];
@@ -152,26 +164,39 @@ pub fn deserialize_raw_to_struct_plr(data: Vec<u8>) -> Plr {
         name: name,
         difficulty: difficulty,
         play_time: play_time,
-        UNKNOWN2: unknown2,
+        appearance: Appearance {
+            hair_style: hair_style,
+            hair_dye: hair_dye,
+            hide_visual: hide_visual,
+            hide_misc: hide_misc,
+            gender: gender,
+            hair_colour: hair_colour,
+            skin_colour: skin_colour,
+            eye_colour: eye_colour,
+            shirt_colour: shirt_colour,
+            undershirt_colour: undershirt_colour,
+            pants_colour: pants_colour,
+            shoes_colour: shoes_colour },
         stat_life: stat_life,
         stat_life_max: stat_life_max,
         stat_mana: stat_mana,
         stat_mana_max: stat_mana_max,
         extra_accessory: extra_accessory,
-        UNKNOWN3: unknown3,
+        done_dundefii_event: done_dundefii_event,
         tax_money: tax_money,
-        UNKNOWN4: unknown4,
+        // appearance: Appearance{}, // The rest of the appearance
+        UNKNOWN2: unknown2,
         armor: armor,
         accessories: accessories,
-        UNKNOWN5: unknown5,
+        UNKNOWN3: unknown3,
         accessories_vanity: accessories_vanity,
-        UNKNOWN6: unknown6,
+        UNKNOWN4: unknown4,
         dyes: dyes,
-        UNKNOWN7: unknown7,
+        UNKNOWN5: unknown5,
         inventory1: inventory1,
         inventory2: inventory2,
         byte_length: byte_length,
-        UNKNOWN8: data[byte_length..].to_vec()
+        UNKNOWN6: data[byte_length..].to_vec()
     };
 
     return plr;
@@ -194,7 +219,11 @@ pub fn serialize_struct_to_raw_plr(plr: &Plr) -> Vec<u8> {
         PlrPacker::w_i8(&mut writer, plr.difficulty);
         PlrPacker::w_i64(&mut writer, plr.play_time);
 
-        PlrPacker::w_bytes(&mut writer, plr.UNKNOWN2.to_owned());
+        PlrPacker::w_i32(&mut writer, plr.appearance.hair_style);
+        PlrPacker::w_u8(&mut writer, plr.appearance.hair_dye);
+        PlrPacker::w_u16(&mut writer, plr.appearance.hide_visual);
+        PlrPacker::w_u8(&mut writer, plr.appearance.hide_misc);
+        PlrPacker::w_u8(&mut writer, plr.appearance.gender);
 
         PlrPacker::w_i32(&mut writer, plr.stat_life);
         PlrPacker::w_i32(&mut writer, plr.stat_life_max);
@@ -202,24 +231,32 @@ pub fn serialize_struct_to_raw_plr(plr: &Plr) -> Vec<u8> {
         PlrPacker::w_i32(&mut writer, plr.stat_mana_max);
         PlrPacker::w_bool(&mut writer, plr.extra_accessory);
 
-        PlrPacker::w_u8(&mut writer, plr.UNKNOWN3);
+        PlrPacker::w_bool(&mut writer, plr.done_dundefii_event);
 
         PlrPacker::w_i32(&mut writer, plr.tax_money);
 
-        PlrPacker::w_bytes(&mut writer, plr.UNKNOWN4.to_owned());
+        PlrPacker::w_i32(&mut writer, plr.appearance.hair_colour);
+        PlrPacker::w_i32(&mut writer, plr.appearance.skin_colour);
+        PlrPacker::w_i32(&mut writer, plr.appearance.eye_colour);
+        PlrPacker::w_i32(&mut writer, plr.appearance.shirt_colour);
+        PlrPacker::w_i32(&mut writer, plr.appearance.undershirt_colour);
+        PlrPacker::w_i32(&mut writer, plr.appearance.pants_colour);
+        PlrPacker::w_i32(&mut writer, plr.appearance.shoes_colour);
+        
+        PlrPacker::w_bytes(&mut writer, plr.UNKNOWN2.to_owned());
 
         plr.armor.map(|e| {PlrPacker::w_i32(&mut writer, e.id); PlrPacker::w_u8(&mut writer, e.prefix)});
         plr.accessories.map(|e| {PlrPacker::w_i32(&mut writer, e.id); PlrPacker::w_u8(&mut writer, e.prefix)});
         
-        PlrPacker::w_bytes(&mut writer, plr.UNKNOWN5.to_owned());
+        PlrPacker::w_bytes(&mut writer, plr.UNKNOWN3.to_owned());
         
         plr.accessories_vanity.map(|e| {PlrPacker::w_i32(&mut writer, e.id); PlrPacker::w_u8(&mut writer, e.prefix)});
 
-        PlrPacker::w_bytes(&mut writer, plr.UNKNOWN6.to_owned());
+        PlrPacker::w_bytes(&mut writer, plr.UNKNOWN4.to_owned());
 
         plr.dyes.map(|e| {PlrPacker::w_i32(&mut writer, e.id); PlrPacker::w_u8(&mut writer, e.prefix)});
 
-        PlrPacker::w_bytes(&mut writer, plr.UNKNOWN7.to_owned());
+        PlrPacker::w_bytes(&mut writer, plr.UNKNOWN5.to_owned());
 
         plr
             .inventory1
@@ -244,7 +281,7 @@ pub fn serialize_struct_to_raw_plr(plr: &Plr) -> Vec<u8> {
             )
         ;
 
-        PlrPacker::w_bytes(&mut writer, plr.UNKNOWN8.to_owned());
+        PlrPacker::w_bytes(&mut writer, plr.UNKNOWN6.to_owned());
     }
 
     return data;
@@ -260,7 +297,7 @@ pub fn deconstruct_plr(filepath: &str, key: &[u8]) -> Plr {
 }
 
 pub fn reconstruct_plr(plr: &mut Plr, key: &[u8]) {
-    plr.name = "abcdef".to_string();
+    plr.name = "M4r4ud3r".to_string();
     let raw = serialize_struct_to_raw_plr(plr);
     let encrypted = encrypt_plr_aes128cbc(raw, key);
     write(format!("./{}.plr.bak.bak", plr.name), encrypted).expect("Error serializing PLR");
@@ -268,7 +305,6 @@ pub fn reconstruct_plr(plr: &mut Plr, key: &[u8]) {
 
 struct PlrUnpacker;
 
-#[allow(unused)]
 impl PlrUnpacker {
     pub fn r_i8<R: Read + Seek>(reader: &mut R) -> i8 {
         return reader.read_i8().expect("Error reading byte as int8");
@@ -276,6 +312,10 @@ impl PlrUnpacker {
 
     pub fn r_u8<R: Read + Seek>(reader: &mut R) -> u8 {
         return reader.read_u8().expect("Error reading byte as uint8");
+    }
+
+    pub fn r_u16<R: Read + Seek>(reader: &mut R) -> u16 {
+        return reader.read_u16::<LittleEndian>().expect("Error reading bytes as uint16");
     }
 
     pub fn r_i32<R: Read + Seek>(reader: &mut R) -> i32 {
@@ -303,14 +343,13 @@ impl PlrUnpacker {
 
     pub fn r_string<R: Read + Seek>(reader: &mut R, l: usize) -> String {
         let mut buf = vec![0; l];
-        let n = reader.read_exact(&mut buf).expect("Error reading bytes");
+        let _n = reader.read_exact(&mut buf).expect("Error reading bytes");
         return String::from_utf8(buf).expect("Error converting byte vector into String");
     }
 }
 
 struct PlrPacker;
 
-#[allow(unused)]
 impl PlrPacker {
     pub fn w_i8<W: Write + Seek>(writer: &mut W, v: i8) -> usize {
         writer.write_i8(v).expect("Error writing signed byte to buffer");
@@ -320,6 +359,11 @@ impl PlrPacker {
     pub fn w_u8<W: Write + Seek>(writer: &mut W, v: u8) -> usize {
         writer.write_u8(v).expect("Error writing unsigned byte to buffer");
         return 1;
+    }
+
+    pub fn w_u16<W: Write + Seek>(writer: &mut W, v: u16) -> usize {
+        writer.write_u16::<LittleEndian>(v).expect("Error writing int32 to buffer");
+        return 2;
     }
 
     pub fn w_i32<W: Write + Seek>(writer: &mut W, v: i32) -> usize {
@@ -360,6 +404,30 @@ impl PlrPacker {
     Clone,
     Copy,
 )]
+pub struct Appearance {
+    pub hair_style: i32,
+    pub hair_dye: u8,
+    pub hide_visual: u16,
+    pub hide_misc: u8,
+    pub gender: u8,
+
+    pub hair_colour: i32,
+    pub skin_colour: i32,
+    pub eye_colour: i32,
+    pub shirt_colour: i32,
+    pub undershirt_colour: i32,
+    pub pants_colour: i32,
+    pub shoes_colour: i32,
+}
+
+#[derive(
+    Serialize,
+    Deserialize,
+    Debug,
+    Default,
+    Clone,
+    Copy,
+)]
 pub struct Equipment {
     pub id: i32,
     pub prefix: u8
@@ -380,6 +448,7 @@ pub struct Item {
     pub favourites: bool
 }
 
+#[allow(non_snake_case)]
 #[derive(
     Serialize,
     Deserialize,
@@ -389,55 +458,61 @@ pub struct Plr {
     pub version: u32,
     pub company: String,
     pub file_type: u8,
-    pub UNKNOWN1: Vec<u8>,
+    pub UNKNOWN1: Vec<u8>, // 12 bytes
     pub name_length: u8,
     pub name: String,
     pub difficulty: i8,
     pub play_time: i64,
-    pub UNKNOWN2: Vec<u8>,
+    pub appearance: Appearance,
     pub stat_life: i32,
     pub stat_life_max: i32,
     pub stat_mana: i32,
     pub stat_mana_max: i32,
     pub extra_accessory: bool,
-    pub UNKNOWN3: u8,
+    pub done_dundefii_event: bool,
     pub tax_money: i32,
-    pub UNKNOWN4: Vec<u8>,
+    // pub appearance: Appearance, // The rest of the appearance
+    pub UNKNOWN2: Vec<u8>, // 10 bytes
     pub armor: [Equipment; 3],
     // armor: [(Equipment, Equipment, Equipment)],
     pub accessories: [Equipment; 6],
     // accessories: [(Equipment, Equipment, Equipment, Equipment, Equipment, Equipment)],
-    pub UNKNOWN5: Vec<u8>,
+    pub UNKNOWN3: Vec<u8>, // 20 bytes
     pub accessories_vanity: [Equipment; 6],
     // accessories_vanity: [(Equipment, Equipment, Equipment, Equipment, Equipment, Equipment)],
-    pub UNKNOWN6: Vec<u8>,
+    pub UNKNOWN4: Vec<u8>, // 5 bytes
     pub dyes: [Equipment; 8],
     // dyes: [(Equipment, Equipment, Equipment, Equipment, Equipment, Equipment, Equipment, Equipment)],
-    pub UNKNOWN7: Vec<u8>,
+    pub UNKNOWN5: Vec<u8>, // 10 bytes
     pub inventory1: [Item; 32],
     pub inventory2: [Item; 26],
-    pub byte_length: usize,
-    pub UNKNOWN8: Vec<u8>
+    pub UNKNOWN6: Vec<u8>, // Stuff like misc_equipments(id, prefix) misc_dyes(id, prefix) piggy_bank(id, stack, prefix) safe(id, stack, prefix) buffs(id, duration) world_list(if_-1_skip, spawn_x, spawn_y, address, name) hotbar_locked(bool) hide_statuses_under_map(bool[]) fishing_quests_completed(int32) padding dpa_bindings(int32) builder_account_status(int32) bartender_quests(int32) mod_data padding
+    pub byte_length: usize
 }
 
 impl Plr {
+    #[allow(unused)]
     fn print_plr(plr: &Plr) {
         println!(
             "
             version:\t\t{}
             company:\t\t{}
             file_type:\t\t{}
+            UNKNOWN DATA
             name_length:\t{}
             name:\t\t{}
             difficulty:\t\t{}
             play_time:\t\t{}
+            appearance:\t\t{:?}
             stat_life:\t\t{}
             stat_life_max:\t{}
             stat_mana:\t\t{}
             stat_mana_max:\t{}
             extra_accessory:\t{}
+            done_dundefii_event:\t{}
             tax_money:\t\t{}
-            byte_length:\t{}
+            OTHER HALF OF APPEARANCE
+            UNKNOWN DATA
             ",
             plr.version,
             plr.company,
@@ -446,13 +521,14 @@ impl Plr {
             plr.name,
             plr.difficulty,
             plr.play_time,
+            plr.appearance,
             plr.stat_life,
             plr.stat_life_max,
             plr.stat_mana,
             plr.stat_mana_max,
             plr.extra_accessory,
+            plr.done_dundefii_event,
             plr.tax_money,
-            plr.byte_length,
         );
         plr.armor.map(|e| println!("armor:\t{:?}", e));
         plr.accessories.map(|e| println!("accessories:\t{:?}", e));
@@ -460,5 +536,6 @@ impl Plr {
         plr.dyes.map(|e| println!("dyes:\t{:?}", e));
         plr.inventory1.map(|i| println!("{:?}", i));
         plr.inventory2.map(|i| println!("{:?}", i));
+        println!("UNKNOWN DATA\nNumber of bytes:{}", plr.byte_length);
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -9,7 +9,6 @@ fn main() {
     dotenv().expect("Error: .env file not found");
 
     let key = env::var("key").expect("Error key not found in env").encode_utf16().flat_map(|x| x.to_le_bytes()).collect::<Vec<u8>>();
-    println!("{:x?}", &key);
     let key = key.as_slice();
     let mut plr = edit_plr::deconstruct_plr(&args[1], key);
     edit_plr::reconstruct_plr(&mut plr, key);
